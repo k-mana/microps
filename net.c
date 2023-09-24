11d03597a4f75efe662792b014b509a962bd8a84
k-mana <k-mana@mve.biglobe.ne.jp>
2023-09-25 03:25:22 +0900

    step22 complete

--- net.c
+++ net.c
@@ -362,6 +362,10 @@ net_init(void)
         errorf("udp_init() failure");
         return -1;
     }
+    if (tcp_init() == -1) {
+        errorf("tcp_init() failure");
+        return -1;
+    }
     infof("initialized");
     return 0;
 }
